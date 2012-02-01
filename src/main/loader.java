4b70183fbb7501da9b7bf83dcbee812c64ff4864
Jerven Bolleman <jerven.bolleman@isb-sib.ch>
2012-02-01 14:24:34 +0100

    signal pusher threads when all data loaded

--- src/main/loader.java
+++ src/main/loader.java
@@ -40,134 +40,136 @@ import com.github.sesameloader.StatementIntoQueuePusher;
 import com.github.sesameloader.owlim.OwlimRepositoryManager;
 import com.github.sesameloader.sesame.NativeRepositoryManager;
 
-
 public class loader
 {
-	private final BlockingQueue<Statement> queue = new ArrayBlockingQueue<Statement>(1000);
-	Logger log = LoggerFactory.getLogger(loader.class);
-	volatile boolean finished = false;
-	private ExecutorService exec;
-	private final RepositoryManager manager;
 
-	public loader(File dataDir, Integer commitXStatements, Integer threads, String providerType) throws SailException,
-	    RepositoryException
-	{
+    private final BlockingQueue<Statement> queue = new ArrayBlockingQueue<Statement>(1000);
+    private final Logger log = LoggerFactory.getLogger(loader.class);
+    volatile boolean finished = false;
+    private ExecutorService exec;
+    private final RepositoryManager manager;
+    private final List<StatementFromQueueIntoRepositoryPusher> pushers = new ArrayList<StatementFromQueueIntoRepositoryPusher>();
 
-		manager = getRepositoryManager(dataDir, providerType);
-		createPushers(commitXStatements, threads, manager);
-	}
+    public loader(File dataDir, Integer commitXStatements, Integer threads, String providerType) throws SailException,
+            RepositoryException
+    {
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 * @throws SailException
-	 * @throws RepositoryException
-	 */
-	public static void main(String[] args)
-	    throws FileNotFoundException, IOException, SailException, RepositoryException
-	{
+        manager = getRepositoryManager(dataDir, providerType);
+        createPushers(commitXStatements, threads, manager);
+    }
 
-		OptionParser parser = new OptionParser();
-		OptionSpec<File> infile = parser.accepts("infile").withRequiredArg().ofType(File.class).required();
-		OptionSpec<File> dataFile = parser.accepts("dataFile").withRequiredArg().ofType(File.class).required();
-		OptionSpec<String> baseUri = parser.accepts("baseUri").withRequiredArg().ofType(String.class).required();
-		OptionSpec<Integer> commitEveryXStatements = parser.accepts("commitInterval").withRequiredArg().required()
-		    .ofType(Integer.class);
-		OptionSpec<Integer> threads = parser.accepts("pushThreads").withRequiredArg().ofType(Integer.class).required();
-		OptionSpec<String> dataBaseProvider = parser.accepts("databaseProvider").withRequiredArg().ofType(String.class)
-		    .required();
+    /**
+     * @param args
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws SailException
+     * @throws RepositoryException
+     */
+    public static void main(String[] args)
+            throws FileNotFoundException, IOException, SailException, RepositoryException
+    {
 
-		OptionSet options = parser.parse(args);
-		if (options.has(infile) && options.has(dataFile) && options.has(baseUri) && options.has(commitEveryXStatements)
-		    && options.has(threads))
-		{
-			final loader loader = new loader(options.valueOf(dataFile), options.valueOf(commitEveryXStatements),
-			    options.valueOf(threads), options.valueOf(dataBaseProvider));
-			loader.load(options.valueOf(infile), options.valueOf(baseUri));
-		}
-	}
+        OptionParser parser = new OptionParser();
+        OptionSpec<File> infile = parser.accepts("infile").withRequiredArg().ofType(File.class).required();
+        OptionSpec<File> dataFile = parser.accepts("dataFile").withRequiredArg().ofType(File.class).required();
+        OptionSpec<String> baseUri = parser.accepts("baseUri").withRequiredArg().ofType(String.class).required();
+        OptionSpec<Integer> commitEveryXStatements = parser.accepts("commitInterval").withRequiredArg().required().ofType(Integer.class);
+        OptionSpec<Integer> threads = parser.accepts("pushThreads").withRequiredArg().ofType(Integer.class).required();
+        OptionSpec<String> dataBaseProvider = parser.accepts("databaseProvider").withRequiredArg().ofType(String.class).required();
 
-	private RepositoryManager getRepositoryManager(File dataFileLocation, String databaseProvider)
-	    throws RepositoryException, SailException
-	{
+        OptionSet options = parser.parse(args);
+        if (options.has(infile) && options.has(dataFile) && options.has(baseUri) && options.has(commitEveryXStatements)
+                && options.has(threads))
+        {
+            final loader loader = new loader(options.valueOf(dataFile), options.valueOf(commitEveryXStatements),
+                    options.valueOf(threads), options.valueOf(dataBaseProvider));
+            loader.load(options.valueOf(infile), options.valueOf(baseUri));
+        }
+    }
 
-		if ("native".equalsIgnoreCase(databaseProvider))
-			return new NativeRepositoryManager(dataFileLocation);
-		else if ("owlim".equalsIgnoreCase(databaseProvider))
-			return new OwlimRepositoryManager(dataFileLocation);
-		else
-			throw new RuntimeException("Don't know databaseProvider");
-	}
+    private RepositoryManager getRepositoryManager(File dataFileLocation, String databaseProvider)
+            throws RepositoryException, SailException
+    {
 
-	private void createPushers(Integer commitEveryXStatements, int threads, RepositoryManager connection)
-	    throws RepositoryException
-	{
-		exec = Executors.newFixedThreadPool(threads);
-		List<Future<?>> futures = new ArrayList<Future<?>>();
-		for (int i = 0; i < threads; i++)
-			futures.add(exec.submit(new StatementFromQueueIntoRepositoryPusher(queue, commitEveryXStatements,
-			    connection)));
-	}
+        if ("native".equalsIgnoreCase(databaseProvider))
+            return new NativeRepositoryManager(dataFileLocation);
+        else if ("owlim".equalsIgnoreCase(databaseProvider))
+            return new OwlimRepositoryManager(dataFileLocation);
+        else
+            throw new RuntimeException("Don't know databaseProvider");
+    }
 
-	private void load(File file, String baseUri)
-	    throws FileNotFoundException, IOException, RepositoryException, SailException
-	{
+    private void createPushers(Integer commitEveryXStatements, int threads, RepositoryManager connection)
+            throws RepositoryException
+    {
+        exec = Executors.newFixedThreadPool(threads);
 
-		try
-		{
-			if (file.isDirectory())
-			{
-				for (File infile : file.listFiles())
-					loadFile(infile, baseUri);
-			}
-			else
-				loadFile(file, baseUri);
-			exec.shutdown();
-			while (!exec.isTerminated())
-				try
-				{
-					exec.awaitTermination(1, TimeUnit.SECONDS);
-				} catch (InterruptedException e)
-				{
-					Thread.currentThread().interrupt();
-				}
-		} finally
-		{
-			manager.shutDown();
-		}
-	}
+        for (int i = 0; i < threads; i++)
+        {
+            final StatementFromQueueIntoRepositoryPusher statementFromQueueIntoRepositoryPusher = new StatementFromQueueIntoRepositoryPusher(queue, commitEveryXStatements,
+                    connection);
+            pushers.add(statementFromQueueIntoRepositoryPusher);
+            exec.submit(statementFromQueueIntoRepositoryPusher);
+        }
+    }
 
-	private void loadFile(File file, String baseUri)
-	    throws FileNotFoundException, IOException, RepositoryException, SailException
-	{
-		final String name = file.getName();
-		if (name.endsWith(".gz"))
-			load(new GZIPInputStream(new FileInputStream(file)), name.substring(0, name.length() - 3), baseUri);
-		else
-			load(new FileInputStream(file), name, baseUri);
-	}
+    private void load(File file, String baseUri)
+            throws FileNotFoundException, IOException, RepositoryException, SailException
+    {
 
-	private void load(InputStream stream, String filename, String baseUri)
-	    throws IOException, RepositoryException
-	{
-		RDFFormat format = RDFFormat.forFileName(filename);
-		RDFParser rdfParser = Rio.createParser(format);
-                rdfParser.setValueFactory(manager.getConnection().getValueFactory());
-		rdfParser.setRDFHandler(new StatementIntoQueuePusher(queue));
-		try
-		{
-			rdfParser.parse(stream, baseUri);
-		} catch (RDFParseException e)
-		{
-			log.error(e.getMessage());
-		} catch (RDFHandlerException e)
-		{
-			log.error(e.getMessage());
-		} finally
-		{
-			finished = true;
-			log.info(filename + "read");
-		}
-	}
+        try
+        {
+            if (file.isDirectory())
+                for (File infile : file.listFiles())
+                    loadFile(infile, baseUri);
+            else
+                loadFile(file, baseUri);
+            for (StatementFromQueueIntoRepositoryPusher pusher:pushers)
+                pusher.setFinished(true);
+            exec.shutdown();
+            while (!exec.isTerminated())
+                try
+                {
+                    exec.awaitTermination(1, TimeUnit.SECONDS);
+                } catch (InterruptedException e)
+                {
+                    Thread.currentThread().interrupt();
+                }
+        } finally
+        {
+            manager.shutDown();
+        }
+    }
+
+    private void loadFile(File file, String baseUri)
+            throws FileNotFoundException, IOException, RepositoryException, SailException
+    {
+        final String name = file.getName();
+        if (name.endsWith(".gz"))
+            load(new GZIPInputStream(new FileInputStream(file)), name.substring(0, name.length() - 3), baseUri);
+        else
+            load(new FileInputStream(file), name, baseUri);
+    }
+
+    private void load(InputStream stream, String filename, String baseUri)
+            throws IOException, RepositoryException
+    {
+        RDFFormat format = RDFFormat.forFileName(filename);
+        RDFParser rdfParser = Rio.createParser(format);
+        rdfParser.setValueFactory(manager.getConnection().getValueFactory());
+        rdfParser.setRDFHandler(new StatementIntoQueuePusher(queue));
+        try
+        {
+            rdfParser.parse(stream, baseUri);
+        } catch (RDFParseException e)
+        {
+            log.error(e.getMessage());
+        } catch (RDFHandlerException e)
+        {
+            log.error(e.getMessage());
+        } finally
+        {
+            log.info(filename + "read");
+        }
+    }
 }
