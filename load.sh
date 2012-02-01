1228664fb924ee959dce60e11c4c732373afa465
Jerven Bolleman <jerven.bolleman@isb-sib.ch>
2012-02-01 15:21:17 +0100

    Shell scripts making it easier to work.

---
+++ load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit # -e does not work in Shebang-line!
+set -o pipefail
+set -o nounset
+
+usage="USAGE: $0 [input file] [database directory] [commit interval] [threads] [baseUri]"
+: ${5? $usage}
+
+
+mvn install
+cp="";
+for i in `find ~/.m2/ -name *.jar`;do
+    cp="$cp:$i";
+done;
+
+java -classpath "target/sesame-loader-0.1.0-SNAPSHOT.jar$cp:conf/" loader -infile $1 -dataFile $2 -databaseProvider native -commitInterval $3 -pushThreads $4 -baseUri $5
---
+++ load_owlim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit # -e does not work in Shebang-line!
+set -o pipefail
+set -o nounset
+
+usage="USAGE: $0 [input file] [database directory] [owlim/native] [commit interval] [threads] [baseUri] [owlim.jar] [crypto.jar]"
+: ${8? $usage}
+
+
+mvn install
+cp="";
+for i in `find ~/.m2/ -name *.jar`;do
+    if [[ ! "$i" =~ "owlim-shim.*" ]]; then
+        cp="$cp:$i";
+    else
+        echo "Excluding $i"
+    fi
+done;
+
+cp="$cp:$7:$8"
+echo $cp
+java -classpath "target/sesame-loader-0.1.0-SNAPSHOT.jar$cp:conf/" loader -infile $1 -dataFile $2 -databaseProvider $3 -commitInterval $4 -pushThreads $5 -baseUri $6
