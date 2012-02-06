6eb1367de3bba88312ccf11a0921892f9e564a66
Jerven Bolleman <jerven.bolleman@isb-sib.ch>
2012-02-06 13:51:13 +0100

    If we have an exception in reading set the the pushers to finished.

--- src/main/loader.java
+++ src/main/loader.java
@@ -137,6 +137,8 @@ public class loader
                 }
         } finally
         {
+            for (StatementFromQueueIntoRepositoryPusher pusher:pushers)
+                pusher.setFinished(true);
             manager.shutDown();
         }
     }
