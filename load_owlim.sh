230c5f6e30fef8cb0ccd0dda95ac7c36c0f960e7
Peter Ansell <p_ansell@yahoo.com>
2012-03-19 14:25:20 +1000

    add mvn clean to load scripts

--- load_owlim.sh
+++ load_owlim.sh
@@ -6,7 +6,6 @@ set -o nounset
 usage="USAGE: $0 [input file] [database directory] [commit interval] [threads] [baseUri]"
 : ${5? $usage}
 
-
-mvn install
+mvn clean install
 
 sh sesame-loader-runtime/dist/bin/load-owlim -infile $1 -dataFile $2 -commitInterval $3 -pushThreads $4 -baseUri $5
