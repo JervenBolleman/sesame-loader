7735a4c5e793913d9f6f2255d2b236eb6eddcde4
Peter Ansell <p_ansell@yahoo.com>
2012-03-19 14:19:12 +1000

    load.sh and load_owlim.sh changes and README updates

--- load.sh
+++ load.sh
@@ -8,9 +8,5 @@ usage="USAGE: $0 [input file] [database directory] [commit interval] [threads] [
 
 
 mvn install
-cp="";
-for i in `find ~/.m2/ -name *.jar`;do
-    cp="$cp:$i";
-done;
 
-java -classpath "sesame-loader-main/target/sesame-loader-main-0.1.0-SNAPSHOT.jar$cp:conf/" loader -infile $1 -dataFile $2 -databaseProvider native -commitInterval $3 -pushThreads $4 -baseUri $5
+sh sesame-loader-runtime/dist/bin/load-native -infile $1 -dataFile $2 -commitInterval $3 -pushThreads $4 -baseUri $5
